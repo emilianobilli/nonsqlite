856bbf922ed14b575e6dd892c1449a9fe287e1df
Nicolas Pajoni <npajoni@claxson.com>
2016-09-05 12:37:23 -0300

    PIP Package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nonsqlite',
+      version='0.2',
+      description='Non SQL Database',
+      url='https://github.com/emilianobilli/nonSQLite',
+      author='Emiliano Billi',
+      author_email='emiliano.billi@gmail.com',
+      license='GPL',
+      packages=['nonsqlite'],
+      zip_safe=False)
